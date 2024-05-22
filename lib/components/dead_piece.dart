16cc4a8b43e445aa0759c2676940583b22e70842
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-05-23 01:19:28 +0530

    dead piece

---
+++ lib/components/dead_piece.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class DeadPiece extends StatelessWidget {
+  final String imagePath;
+  final bool isWhite;
+  const DeadPiece({
+    super.key,
+    required this.imagePath,
+    required this.isWhite,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 5),
+      child: Image.asset(
+        imagePath,
+        color: isWhite ? Colors.grey[300] : Colors.grey[800],
+      ),
+    );
+  }
+}
